faa39d5b0965b6cb63bcfbf6725c5a6a87444233
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-07-24 21:18:26 +0530

    More on recursion

--- recursion.cpp
+++ recursion.cpp
@@ -60,11 +60,34 @@ void printArray(vector<int> a, int n){ // without index passing
     printArray(a, n-1);
     cout << a[n-1] << " ";
 }
-int maxElement(vector<int> a, int n, int max){      // will do dry run and not giving accurate answers
-    if (n<0) return max;
-    max = a[n-1] > max ? a[n-1] : max; 
-    return maxElement(a, n-1, max);
+int maxElement(vector<int> a, int n, int max, int it){      // will do dry run and not giving accurate answers
+    if (it>=n) return max;
+    max = a[it] > max ? a[it] : max;
+    return maxElement(a, n, max, it+1);
+    
+    // if (n<0) return max; 
+    // max = a[n-1] > max ? a[n-1] : max; 
+    // return maxElement(a, n-1, max, it);
 }
+int minElement(int a[], int n, int min, int i){
+    if (i>=n) return min;
+    min = a[i] < min ? a[i] : min;
+    return minElement(a, n, min, i+1);
+}
+int sumOfDigits(int n){
+    if (n<=0) return 0;
+    return n%10+sumOfDigits(n/10);
+}
+int powerRec(int a, int b){
+    if (b==0) return 1;
+    return powerRec(a, b-1) * a;
+}
+bool keyPresent(string s, int n, int i, char key){
+    if (i>=n) return false;
+    if (s[i] == key) return true;
+    return keyPresent(s, n, i+1, key);
+}
+
 int main(){
     /* 
     // print using recursion
@@ -90,8 +113,6 @@ int main(){
 
 
 
-
-
     /* // climbing stairs  -- dry run
     int n=3;
     cout << climbingStairs(n) << endl; */
@@ -103,10 +124,43 @@ int main(){
     printArray(v, v.size()); */
 
 
-    // max element in the array
-    vector<int> v = {1,2,3,90,12,321};
+    /* // max element in the array
+    vector<int> v = {1, 5, 2, 99, 12};
     int max = INT_MIN;
-    cout << maxElement(v, v.size(), max);
+    int i=0;
+    cout << maxElement(v, v.size(), max, i); */
+
+
+    /* // sum of digits
+    int n=123;
+    cout << sumOfDigits(n) << endl; */
+
+
+    /* // power of a number
+    int a = 2;
+    int b = 6;
+    cout << powerRec(a, b) << endl; */
+
+    /* // minimum element in the array
+    int a[] = {76,32,56,123, -10,77, -1,54, 11};
+    int size = sizeof(a)/sizeof(int);
+    int min = INT_MAX;
+    int i=0;
+    cout << minElement(a, size, min, i); */
+
+
+    /* // key present or not in a string
+    string s = "shivampahuja";
+    int i=0;
+    char key = 'm';
+    cout << keyPresent(s, s.size(), i, key) << endl; */
+
+
+
+
+
+
+
 }
 
 

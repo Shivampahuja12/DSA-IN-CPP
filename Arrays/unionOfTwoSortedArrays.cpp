fb4f6f3cdecdaaba1992b8238126bf6fb6b1ccfe
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-07 13:45:03 +0530

    Union Of Two Sorted Arrays Add

---
+++ Arrays/unionOfTwoSortedArrays.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+using namespace std;
+
+vector<int> unionNoDup(vector<int> &a, vector<int> &b){
+    vector<int> ans;
+    for (int i=0; i<a.size(); i++){
+        ans.push_back(a[i]);
+    }
+    for (int i=0; i<b.size(); i++){
+        ans.push_back(b[i]);
+    }
+    return ans;
+}
+
+vector<int> bruteUnionOfTwoArrays(vector<int> a, vector<int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    set<int> st;
+    for (int i=0; i<n1; i++){
+        st.insert(a[i]);
+    }
+    for (int i=0; i<n2; i++){
+        st.insert(b[i]);
+    }
+
+    vector<int> temp;
+    for (auto it: st){
+        temp.push_back(it);
+    }
+    return temp;
+}
+
+vector<int> optimalUnionOfTwoArrays(vector<int> a, vector<int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    int i=0;
+    int j=0;
+
+    vector<int> unionArr;
+    while (i<n1 && j<n2){
+        if (a[i] <= b[j]){
+            if (unionArr.size() == 0 || unionArr.back() != a[i]){
+            unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else {
+            if (unionArr.size() == 0 || unionArr.back() != b[j]){
+            unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+        
+    }
+    
+    while (j<n2){
+        if (unionArr.size() == 0 || unionArr.back() != b[j]){
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+    
+    while (i<n1){
+        if (unionArr.size() == 0 || unionArr.back() != a[i]){
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+
+
+    return unionArr;
+}
+
+
+int main(){
+    vector<int> a = {1, 3, 4,5,6,9};
+    vector<int> b = {2,3,11};
+
+    // vector<int> ans = unionNoDup(a, b);
+    // sort(ans.begin(), ans.end());
+
+    vector<int> ans1 = bruteUnionOfTwoArrays(a,b);
+    vector<int> ans2 = optimalUnionOfTwoArrays(a,b);
+
+    for (int i: ans2) {
+        cout << i << " ";
+    }
+}
